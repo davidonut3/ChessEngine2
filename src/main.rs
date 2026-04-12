6543ad580344c5aa3cca3e60fa83c5d819d69244
davidonut3 <david@martinengeanne.nl>
2026-04-12 11:09:06 +0200

    Built CLI skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::io;
+
+const HELP: &str = "help";
+const QUIT: &str = "quit";
+const FEN: &str = "fen";
+
+const COMMAND_HELP: &str = "
+    help \t Get this info.
+    quit \t Quit the application.
+    fen \t Get access to the engine.
+";
+
 fn main() {
-    println!("Hello, world!");
+    println!("Chess Engine Command Line Tool | Enter 'help' for help.");
+
+    loop {
+        let mut user_input = String::new();
+
+        io::stdin()
+            .read_line(&mut user_input)
+            .expect("Failed to read line");
+
+        user_input = user_input.trim().to_lowercase();
+
+        if user_input == HELP {
+            println!("{}", COMMAND_HELP)
+        }
+
+        if user_input == QUIT {
+            break;
+        }
+
+        if user_input == FEN {
+            continue;
+        }
+    }
 }
